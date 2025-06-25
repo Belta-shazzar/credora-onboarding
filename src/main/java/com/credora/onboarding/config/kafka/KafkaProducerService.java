b938fae2953ecadf283d9c9c81adb9ce447077fa
daniel-oguejiofor <d.oguejiofor@maontech.com>
2025-06-25 14:23:06 +0100

    feat: add distributed tracing implementation with kafka message producer

--- src/main/java/com/credora/onboarding/config/kafka/KafkaProducerService.java
+++ src/main/java/com/credora/onboarding/config/kafka/KafkaProducerService.java
@@ -1,12 +1,18 @@
 package com.credora.onboarding.config.kafka;
 
 import com.credora.onboarding.config.kafka.dto.OnboardingNotificationRequest;
+import io.micrometer.tracing.Span;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.internals.RecordHeader;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CompletableFuture;
 
 @Slf4j
@@ -14,39 +20,28 @@ import java.util.concurrent.CompletableFuture;
 @RequiredArgsConstructor
 public class KafkaProducerService {
   private final KafkaTemplate<String, OnboardingNotificationRequest> kafkaTemplate;
-
-//  public void sendOnboardingConfirmationNotification(OnboardingNotificationRequest request) {
-//    System.out.println(request);
-//    log.info("Sending onboarding confirmation event: {}", request);
-//
-//    Message<OnboardingNotificationRequest> message = MessageBuilder
-//            .withPayload(request)
-//            .setHeader(TOPIC, "onboarding-confirmation")
-//            .build();
-//
-//    System.out.println(message);
-//    CompletableFuture<SendResult<String, OnboardingNotificationRequest>> future = kafkaTemplate.send(message);
-//
-//    future.whenComplete((result, ex) -> {
-//      if (ex == null) {
-//        log.info("Message sent successfully to topic: {}, partition: {}, offset: {}",
-//                result.getRecordMetadata().topic(),
-//                result.getRecordMetadata().partition(),
-//                result.getRecordMetadata().offset());
-//      } else {
-//        log.error("Unable to send message due to: {}", ex.getMessage());
-//      }
-//
-//    });
-//  }
+  private final Tracer tracer;
+  private final Propagator propagator;
 
   public void sendOnboardingConfirmationNotification(OnboardingNotificationRequest request) {
-    log.info("Sending onboarding confirmation event: {}", request);
+    Span span = tracer.nextSpan().name("onboarding-confirmation-producer").start();
 
-    try {
+    try (Tracer.SpanInScope ignored = tracer.withSpan(span)) {
+      log.info("Sending onboarding confirmation event: {}", request);
+
+      ProducerRecord<String, OnboardingNotificationRequest> record =
+              new ProducerRecord<>("onboarding-confirmation", request);
+
+      propagator.inject(span.context(), record.headers(), (headers, key, value) ->
+              {
+                assert headers != null;
+                headers.add(new RecordHeader(key, value.getBytes(StandardCharsets.UTF_8)));
+              }
+      );
+
       // Send message directly to the topic instead of using headers
       CompletableFuture<SendResult<String, OnboardingNotificationRequest>> future =
-              kafkaTemplate.send("onboarding-confirmation", request);
+              kafkaTemplate.send(record);
 
       future.whenComplete((result, ex) -> {
         if (ex == null) {
@@ -54,13 +49,19 @@ public class KafkaProducerService {
                   result.getRecordMetadata().topic(),
                   result.getRecordMetadata().partition(),
                   result.getRecordMetadata().offset());
+
+          span.tag("kafka.topic", result.getRecordMetadata().topic());
+          span.tag("kafka.offset", String.valueOf(result.getRecordMetadata().offset()));
         } else {
           log.error("Unable to send message due to: {}", ex.getMessage());
+          span.error(ex);
         }
+        span.end();
       });
     } catch (Exception e) {
-      log.error("Error sending message to Kafka: {}", e.getMessage(), e);
-      throw e; // Re-throw to let the caller handle it
+      span.error(e);
+      span.end();
+      throw e;
     }
   }
 }
